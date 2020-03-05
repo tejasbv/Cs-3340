4042df7927e1fafbc7c6980efe4db2af3a90690c
tejas <34074610+tejasbv@users.noreply.github.com>
2020-03-04 22:17:18 -0600

    Read the digits
    can read digits from the buffer and add ten if it is a 2 digit, Not able to store to the array

--- Hw 4/Solution/Hw4_Tejas.asm
+++ Hw 4/Solution/Hw4_Tejas.asm
@@ -2,7 +2,8 @@
 
 .data
 filename:	.asciiz	"input.txt"
-read:		.asciiz		
+read:		.asciiz	
+array:		.word	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 
 .text
@@ -28,16 +29,62 @@ main:
 	ble	$v0, $0, exit
 	
 ##################################################
+
+	#extracting the integers function call
+	la	$a0, array
+	addi	$a1,$0,20
+	la	$a2, read
+	jal	extractnum
 	
-	la	$a0, read
-	addi	$a0,$a0,20
 	
 	
-	li	$v0,4
-	la	$a0, read
-	syscall
+
 	
+###################################################
+	# exit program	
 exit:	li	$v0,10
 	syscall
+###################################################	
+
+extractnum:
+	#lb	$t0,1($a2)
+	li  	$t3,0
+	li	$t6,48
+	li	$t7,57
+	li 	$t4,2
+	addi	$sp,$sp, -4
+	sw	$ra,($sp)
+	lw	$s5,($a0)
 	
+loop:	beq	$t3,$a1,contMain
+	add	$t1,$t3,$a2
+	lb	$t0, ($t1)
+	addi	$t3,$t3,1
+	add	$t1,$t3,$a2
+	lb	$t2,($t1)
+	jal 	check
+	sw	$v0,($s5)
 	
+increment:
+	addi	$s5,$s5,4
+	addi	$t3,$t3,1
+	j	loop
+
+contMain:	jr	$ra
+	
+check:	
+	addi	$t5,$0,10
+	blt	$t0,$t6,	increment
+	bgt	$t0,$t7,increment
+	blt	$t2,$t6,	return1
+	bgt	$t2,$t7,return1
+	subi	$t0, $t0,48
+	mul	$t0,$t0,$t5
+	subi	$t2, $t2,48
+	add	$v0,$t2,$t0
+	jr	$ra
+	
+return1:	
+	subi	$t0, $t0,48
+	add	$v0,$0,$t0
+	jr	$ra
